7ae3cd530e1a2d3a3f908c8ce8192a408af7e1b3
Dharmesh Bhuva <dharmesh.bhuva@adelaide.edu.au>
2025-01-03 18:08:04 +1030

    added documentation and exported

--- R/plotCovariates.R
+++ R/plotCovariates.R
@@ -1,4 +1,31 @@
-plotCovariates <- function(spe, covariate = c("biology", "ls", "batch"), ...) {
+#' Diagnostic plot of predicted expression for a covariate
+#' 
+#' This function can be used to spatially visualise the library size, biology or batch specific effect modelled for each gene.
+#' 
+#' @param spe a SpatialExperiment object.
+#' @param covariate a character, specifying the type of covariate to be plot: "biology" (default), "ls" to plot the library size effect, and "batch" to plot the batch-specific effect.
+#' @param ... additional parameters to be passed to the [plotSpatial] function.
+#' @return a ggplot2 object
+#' @export
+#'
+#' @examples
+#' library(SpatialExperiment)
+#' library(ggplot2)
+#' 
+#' data(HumanDLPFC)
+#' \donttest{
+#' HumanDLPFC = SpaNorm(HumanDLPFC, sample.p = 0.05, df.tps = 2, tol = 1e-2)
+#' # plot spatial region annotations
+#' p1 <- plotCovariate(HumanDLPFC, covariate = "biology", colour = ENSG00000075624) +
+#'   scale_colour_viridis_c(option = "F")
+#' p1
+#' 
+#' p2 <- plotCovariate(HumanDLPFC, covariate = "ls", colour = ENSG00000075624) +
+#'   scale_colour_viridis_c(option = "F")
+#' p2
+#' }
+#'
+plotCovariate <- function(spe, covariate = c("biology", "ls", "batch"), ...) {
   checkSPE(spe)
   covariate = match.arg(covariate)
 
